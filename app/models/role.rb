f58eff3b21eb67cdf200bba4e186c822249b7934
vishal Thakur <vish75810@gmail.com>
2022-09-09 02:49:18 +0530

    changes role

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,9 +2,9 @@ class Role < ApplicationRecord
 
      has_many :users
 
-     validates :name, presence: true,
-                             uniqueness: {case_sensitive => false},
-                             length: {maximum: 4 , minimum: 100}
+     # validates :name, presence: true,
+     #                         uniqueness: {case_sensitive => false},
+     #                         length: {maximum: 4 , minimum: 100}
 
 
 
