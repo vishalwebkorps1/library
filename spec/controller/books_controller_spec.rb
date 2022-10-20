662c7a96a300c9de06dc8a851227bf822d774b60
vishal Thakur <vish75810@gmail.com>
2022-10-21 02:16:08 +0530

    update rspec when user is authorized or not

--- spec/controller/books_controller_spec.rb
+++ spec/controller/books_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Books::BooksController, type: :controller do
     end
 
 
-    context "when user is signed in" do
+    context "has 200 status code if sign_in" do
       before do
         sign_in(user)
       end
@@ -30,8 +30,19 @@ RSpec.describe Books::BooksController, type: :controller do
       end
     end
 
+    context "has 402 status code if not sign_in" do
+      before do
+        sign_in=!(user)
+      end
+      it "It fetches single record from the book" do
+        get :show, params: {id: book.id}, format: :json
+        data = JSON.parse(response.body)
+        expect(response.status).to eq 401
+      end
+    end
 
-    context "when user is signed in" do
+
+    context "Book Created when Authorized" do
       before do
         sign_in(user)
       end
@@ -41,11 +52,26 @@ RSpec.describe Books::BooksController, type: :controller do
         post :create, params: {book: books_params} , format: :json
         book = JSON.parse(response.body)
         expect(response.status).to eq 200
+
+      end
+    end
+
+    context "Not authorized for book creating" do
+      before do
+        sign_in=!(user)
+      end
+
+      it "Book created successfully" do
+        books_params = {book: {name:"parabhu", author:"indar", description:"this is for testing", user_id:"2", edition:"2", quantity:"3"}}
+        post :create, params: {book: books_params} , format: :json
+        book = JSON.parse(response.body)
+        expect(response.status).to eq 401
+
       end
     end
 
 
-    context "when user is signed in" do
+    context "Updating the resource " do
       before do
         sign_in(user)
       end
@@ -55,11 +81,25 @@ RSpec.describe Books::BooksController, type: :controller do
         put :update, params:books_params, format: :json
         book =JSON.parse(response.body)
         expect(response.status).to eq 200
+
+      end
+    end
+
+    context "Not updating the resource" do
+      before do
+        sign_in=!(user)
+      end
+
+      it "Updating the book" do
+        books_params = {id: book.id, book: {name:"parabhu", author:"indar", description:"this is for testing", user_id:"2", edition:"2", quantity:"3"} }
+        put :update, params:books_params, format: :json
+        book =JSON.parse(response.body)
+        expect(response.status).to eq 401
       end
     end
 
 
-    context "when user is signed in" do
+    context "User is Authorized" do
       before do
         sign_in(user)
       end
@@ -69,6 +109,18 @@ RSpec.describe Books::BooksController, type: :controller do
         delete :destroy, params:books_params, format: :json
         book =JSON.parse(response.body)
         expect(response.status).to eq 200
+      end
+    end
+    context "User is not Authorized" do
+      before do
+        sign_in=!(user)
+      end
+      
+      it "Delete a book" do
+        books_params = {id: book.id, book: {name:"parabhu", author:"indar", description:"this is for testing", user_id:"2", edition:"2", quantity:"3"} }
+        delete :destroy, params:books_params, format: :json
+        book =JSON.parse(response.body)
+        expect(response.status).to eq 401
       end
     end
   end
--- spec/controller/users_controller_spec.rb
+++ spec/controller/users_controller_spec.rb
@@ -2,22 +2,25 @@ require "rails_helper"
 RSpec.describe Users::UsersController, type: :controller do
  
  
-  describe "GET users#index" do
+  describe ".authenticate" do
       let(:role){create(:role, name: "admin")}
       let(:user){create(:user, role: role)}
-    context " when user is signed in" do
-      before do
-        sign_in(user)
+
+
+      context " when user is signed in it show the all users" do
+        before do
+          sign_in(user)
+        end
+
+        it "It fetches all the users" do
+          get :index, format: :json
+          data = JSON.parse(response.body)
+          expect(data.count).to eq User.all.count
+        end
       end
 
-      it "It fetches all the users" do
-        get :index, format: :json
-        data = JSON.parse(response.body)
-        expect(data.count).to eq User.all.count
-      end
-    end
 
-      context " when user is signed in" do
+      context " when user is signed in it show the user" do
         before do
           sign_in(user)
         end
@@ -29,35 +32,34 @@ RSpec.describe Users::UsersController, type: :controller do
         end
       end
 
-
-      context " when user is signed in" do
+      context " when user is signed in it is not update the user" do
         before do
           sign_in(user)
         end
 
-        it "Updating the user" do
+        it " user is not authorized for update" do
           #  user = create(:user)
           put_id = user.id
           user_params = {id: user.id, user: {name:"parabhu",email:"raju@gmail.com", password:"1234567", role_id:"1", address:"ashta"} }
           put :update, params:user_params, format: :json
           user = JSON.parse(response.body)
-          expect(response.status).to eq 200
+          expect(response.status).to eq 422
         end
       end
 
-      context " when user is signed in" do
-        before do
-          sign_in(user)
-        end
-      
-        it "Delete user" do
+        context " when user is signed in it not delete the user" do
+          before do
+            sign_in(user)
+          end
+    
+        it "Its not valid" do
           delete_id = user.id
           user_params = {id: user.id, book: {name:"parabhu",email:"raju@gmail.com", password:"1234567", role_id:"1", address:"ashta"} }
           delete :destroy, params:user_params, format: :json
           user =JSON.parse(response.body)
-          expect(response.status).to eq 200
+          expect(response.status).to eq 422
         end
-     end
+      end
   end
 end
  
